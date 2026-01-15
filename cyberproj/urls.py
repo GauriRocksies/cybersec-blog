f7ecdfb3a7b7e7b22a881f33cd7a63b93c483e20
GauriRocksiess <gauri03uk@gmail.com>
2026-01-16 00:04:12 +0530

    1. Fixed eye ison in login page. 2. Fixed image showing in post.

--- cyberproj/urls.py
+++ cyberproj/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('accounts/signup/', blog_views.signup, name='signup'),
 ]
 
-#if settings.DEBUG:
- #   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
